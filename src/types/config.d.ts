3cd73263c6a11a137bf3fd567d3d996ec79e56c7
liumeng <378338627@qq.com>
2021-03-23 18:03:27 +0800

    202103231803

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -2,3 +2,8 @@ export interface GlobConfig {
   name: string;
   title: string;
 }
+
+export interface UIConfig {
+  label: string;
+  value: string | number;
+}
--- src/views/settings/WarningSetting.vue
+++ src/views/settings/WarningSetting.vue
@@ -1,23 +1,48 @@
 <template>
   <Form>
+    <FormItem label="通知方式" :labelCol="{ span: 4 }" labelAlign="right">
+      <CheckboxGroup v-model:value="currentNoticeType">
+        <Checkbox v-for="(notice, index) in noticeTypes" :key="index" :value="notice.value">
+          {{ notice.label }}
+        </Checkbox>
+      </CheckboxGroup>
+    </FormItem>
+    <FormItem label="预警类型" :labelCol="{ span: 4 }" labelAlign="right">
+      <Input />
+    </FormItem>
     <FormItem label="手机号" :labelCol="{ span: 4 }" labelAlign="right">
       <Input />
     </FormItem>
-    <FormItem label="邮箱" labelCol="3" labelAlign="right">
+    <FormItem label="邮箱" :labelCol="{ span: 4 }" labelAlign="right">
       <Input />
     </FormItem>
   </Form>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { Form, Input } from 'ant-design-vue';
+  import { defineComponent, ref } from 'vue';
+  import { Form, Input, Checkbox } from 'ant-design-vue';
+  import { UIConfig } from '@/types/config';
   export default defineComponent({
     name: 'WarningSetting',
     components: {
       Form,
       FormItem: Form.Item,
       Input,
+      Checkbox,
+      CheckboxGroup: Checkbox.Group,
+    },
+    setup() {
+      const noticeTypes: UIConfig[] = [
+        { label: '短信', value: 'sms' },
+        { label: '邮件', value: 'email' },
+      ];
+      const currentNoticeType = ref([]);
+
+      return {
+        noticeTypes,
+        currentNoticeType,
+      };
     },
   });
 </script>
